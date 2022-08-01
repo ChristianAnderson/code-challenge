a347848bd71ef5fd9748a66db22530350c881cf5
Christian Ramos <sedo.christian@gmail.com>
2022-08-01 03:10:05 -0500

    back button in basic is now added, favs is working in search view

--- src/app/folder/search/search.page.ts
+++ src/app/folder/search/search.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FoodService } from 'src/app/api/food.service';
-import { LoadingController } from '@ionic/angular';
+import { LocalService } from 'src/app/services/local.service';
+
 
 @Component({
   selector: 'app-search',
@@ -13,10 +15,14 @@ export class SearchPage implements OnInit {
   list : [] = [];
   constructor(
     private foodService: FoodService,
-    private loadingCtrl: LoadingController
+    private router: Router,
+    private localStore: LocalService
     ) { }
 
   ngOnInit() {
+    if(this.localStore.getData('currentFood')){
+      this.search(this.localStore.getData('currentFood'))
+    }
   }
   loading : boolean = false; 
   search = (foodName: string) => {
@@ -31,4 +37,44 @@ export class SearchPage implements OnInit {
     }
   }
 
+  goToBasicReport(itemId: string) {
+    this.localStore.saveData('currentFood', itemId)
+    const link = `${this.router.url}/basic-report/`
+    this.router.navigate([link, itemId]);
+  }
+
+  addToFavs(name: string, id: string) {
+    let favsArray : any
+    if(this.localStore.getData('favoriteFood')) {
+      favsArray = JSON.parse(this.localStore.getData('favoriteFood'));
+    } else {
+      favsArray = [];
+    }
+    console.log(favsArray);
+    favsArray.push(
+      { 'name' : name,
+        'fdcId' : id
+     }
+      );
+    this.localStore.saveData('favoriteFood', JSON.stringify(favsArray))
+  }
+
+  deleteFav(id: string) {
+    let favsArray = JSON.parse(this.localStore.getData('favoriteFood'));
+    favsArray = favsArray.filter(function( obj ) {
+      return obj.fdcId !== id;
+    });
+    console.log(favsArray)
+    this.localStore.saveData('favoriteFood', JSON.stringify(favsArray))
+  }
+
+  checkIfItsInFavList(id: string){
+    let favsArray = JSON.parse(this.localStore.getData('favoriteFood'));
+    favsArray = favsArray.filter(function( obj ) {
+      return obj.fdcId == id;
+    });
+    if( favsArray.length > 0)
+      return true;
+    return false; 
+  }
 }
